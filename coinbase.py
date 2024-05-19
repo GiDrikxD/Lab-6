36f044d49b26a3da7980b95c7728e7f168855a32
GiDrikxD <159322273+GiDrikxD@users.noreply.github.com>
2024-05-19 11:57:01 +0300

    coinbase.py

--- coinbase.py
+++ coinbase.py
@@ -1,6 +1,7 @@
 import cbpro
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 
 public_client = cbpro.PublicClient()
 
@@ -9,27 +10,51 @@ products_df = pd.DataFrame(products)
 
 selected_products = ['BTC-USD', 'ETH-USD', 'LTC-USD']
 
+periods = {
+    '1день': {'granularity': 3600, 'start': datetime.now() - timedelta(days=1)},  
+    '1місяць': {'granularity': 21600, 'start': datetime.now() - timedelta(days=30)},  
+    '1рік': {'granularity': 86400, 'start': datetime.now() - timedelta(days=365)},  
+}
+
 data_frames = {}
+
 for product in selected_products:
     data_frames[product] = {}
-    for period in ['1day', '1month', '1year']:
-        historical_data = public_client.get_product_historic_rates(product, granularity=3600, start=None, end=None)
+    for period, params in periods.items():
+        historical_data = []
+        start_time = params['start']
+        end_time = datetime.now()
+        granularity = params['granularity']
+        
+        while start_time < end_time:
+            batch_end_time = start_time + timedelta(seconds=granularity*300)
+            if batch_end_time > end_time:
+                batch_end_time = end_time
+            data = public_client.get_product_historic_rates(product, 
+                                                            granularity=granularity, 
+                                                            start=start_time.isoformat(), 
+                                                            end=batch_end_time.isoformat())
+            historical_data.extend(data)
+            start_time = batch_end_time
+        
+        historical_data.sort(key=lambda x: x[0])
+        
         df = pd.DataFrame(historical_data, columns=['time', 'low', 'high', 'open', 'close', 'volume'])
         df['time'] = pd.to_datetime(df['time'], unit='s')
         df.set_index('time', inplace=True)
         data_frames[product][period] = df
 
-fig, axs = plt.subplots(len(selected_products), len(data_frames[selected_products[0]]), figsize=(20, 15))
+fig, axs = plt.subplots(len(selected_products), len(periods), figsize=(20, 15))
 for i, product in enumerate(selected_products):
-    for j, period in enumerate(data_frames[product]):
+    for j, period in enumerate(periods):
         ax = axs[i, j]
         data_frames[product][period]['close'].plot(ax=ax)
         ax.set_title(f'{product} - {period}')
 plt.tight_layout()
 plt.show()
-    
+
 for product in selected_products:
-    for period in data_frames[product]:
+    for period in periods:
         df = data_frames[product][period]
         print(f'{product} - {period}:')
         print(f'Середнє значення: {df["close"].mean()}')
